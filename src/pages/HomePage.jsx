338adce15146730656d682c4bf9f3c35975371cc
ZiadRefaey <99500161+ZiadRefaey@users.noreply.github.com>
2023-10-16 02:37:36 +0300

    React router with basic navigation

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function HomePage() {
+  return (
+    <div>
+      <PageNav />
+      WorldWise
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+import PageNav from "../components/PageNav";
+
+function NotFound() {
+  return (
+    <div>
+      <p>
+        <PageNav />
+        Page not found 404
+      </p>
+    </div>
+  );
+}
+
+export default NotFound;
